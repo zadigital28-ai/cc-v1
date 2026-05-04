55874c933a0997e03b74112b86cbe68226d0bf26
zadigital28-ai <zadigital28@gmail.com>
2026-05-04 20:15:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
  * CONFIG — Ganti URL ini saat deploy untuk klien baru
  * ============================================================
  */
-window.GAS_URL = 'https://script.google.com/macros/s/AKfycbxpbQoIz71-LkMlKjDi9k7rKID8rnZNo7TpGL1HagiU_b4mTWQQ0z9o-w9VUUCxfKey/exec';
+window.GAS_URL = 'https://script.google.com/macros/s/AKfycbyZ9MbpzwNQsspzWPpAMsDB7BxPHMh7WO3rucTtAshRAr9b2QlUEMWzFgxvRUyBCRoJ/exec';
